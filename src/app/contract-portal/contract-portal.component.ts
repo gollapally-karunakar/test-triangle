9b53b09ff40539dbc5c4f93cb4701a67a57d929d
Gollapally Karunakar <gollapally@blackswan-technologies.com>
2020-01-10 19:42:02 +0530

    done with skils page

--- src/app/contract-portal/contract-portal.component.ts
+++ src/app/contract-portal/contract-portal.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 
-
+declare var $: any;
 @Component({
   selector: 'app-contract-portal',
   templateUrl: './contract-portal.component.html',
@@ -9,16 +9,36 @@ import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 })
 export class ContractPortalComponent implements OnInit {
   firstFormGroup: FormGroup;
-  secondFormGroup: FormGroup;
+  skillFormGroup: FormGroup;
   isOptional = false;
-
+  
+  public skillsToBeAdded:any = [
+    {
+      'title':'Software Developement'
+    },
+    {
+      'title':'Software Developement'
+    },
+    {
+      'title':'Software Developement'
+    },
+    {
+      'title':'Software Developement'
+    },
+    {
+      'title':'Software Developement'
+    },
+    {
+      'title':'Software Developement'
+    }
+  ]
   constructor(private _formBuilder: FormBuilder) { }
 
   ngOnInit() {
     this.firstFormGroup = this._formBuilder.group({
       firstName: ['', Validators.required],
-      lastName: ['', Validators.required],
-      phone : ['', Validators.required],
+      lastName: [''],
+      phone : [''],
       email : ['', Validators.required],
       jobtitle : ['', Validators.required],
       info:[`At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos
@@ -33,9 +53,33 @@ export class ContractPortalComponent implements OnInit {
       location:[''],
       uploadFile:['']
     });
-    this.secondFormGroup = this._formBuilder.group({
-      secondCtrl: ''
+    this.skillFormGroup = this._formBuilder.group({
+      skillCtrl: '',
+      pcertifications:`At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos
+      dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia
+      animi, id est laborum et dolorum fuga. Et harum quidem rerum.
+      
+      Maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut
+      officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque
+      earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptati.`,
+      startDate:'',
+      endDate:'',
+      workLocation:''
     });
   }
 
 }
+
+
+$(document).ready(function(){
+  $(".mat-horizontal-stepper-header-container").css({
+    'background-color':'#2b3a47',
+    'display': 'flex',
+    'justify-content': 'space-around',
+  });
+  $(".mat-step-label").css("color","#fff").css("textAlign","center").css("fontSize","20px");
+  $('.mat-step-icon-state-number').remove();
+  $(".mat-stepper-horizontal-line").remove();
+  $(".mat-step-icon-state-edit").remove();
+  $(".mat-step-label-selected").parent().css("backgroundColor","#3dbbf3");
+})
